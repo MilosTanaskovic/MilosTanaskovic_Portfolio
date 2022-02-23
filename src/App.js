6ae9b3ff8eafdfb181a869fb738b184c6a36a9af
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-02-23 17:12:36 +0100

    set up the portfolio...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GlobalStyle from './globalStyles'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      App Files
+    </>
+  )
+}
+
---
+++ src/data/WorkData.js
@@ -0,0 +1,62 @@
+// work data
+export const Work = [
+    {
+        id:1,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+        
+    },
+    {
+        id:3,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:4,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+       
+    },{
+        id:5,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+        
+    },
+    {
+        id:6,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+        
+    },
+    
+  
+] 
