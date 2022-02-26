c3f11e05966dd06e216114ba59779f5ced7a3a93
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-02-26 21:32:06 +0100

    setup pages, svgs, themes...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Route, Switch } from 'react-router'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from './components/Themes'
 import GlobalStyle from './globalStyles'
+// pages
+import About from './pages/About/About'
+import Blog from './pages/Blog/Blog'
+import Main from './pages/Main'
+import MySkills from './pages/MySkills/MySkills'
+import Work from './pages/Work/Work'
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
-      App Files
+      <ThemeProvider theme={lightTheme}>
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route path='/about' component={About} />
+          <Route  path='/blog' component={Blog} />
+          <Route  path='/work' component={Work} />
+          <Route  path='/skills' component={MySkills} />
+        </Switch>
+      </ThemeProvider>
     </>
   )
 }
